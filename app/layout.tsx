153fdc5f7449eb97a27622b1f10c40a841a3c0a4
Johan le Roch <johanleroch@gmail.com>
2024-05-15 22:15:38 +0200

    feat: integrate new libraries and RootProvider
    Integrate `@mangrovedao/mgv`, `@rainbow-me/rainbowkit`, 
    `@tanstack/react-query`, `viem`, and `wagmi` libraries for enhanced 
    development capabilities and smoother user experiences. Additionally, 
    introduce `RootProvider` component to the app layout for centralized 
    state management, improving the app's overall structure and maintainability. 
    Update dependencies in `package.json` and lock files accordingly to ensure 
    compatibility and stability of the new features.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import RootProvider from "@/providers/root";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <RootProvider>{children}</RootProvider>
+      </body>
     </html>
   );
 }
---
+++ providers/root.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  blast,
+} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+  chains: [blast],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+export default function RootProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
