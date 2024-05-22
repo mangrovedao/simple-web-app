38e51aa58553d774301eb9a2fe5615e2d7835ee4
Johan le Roch <johanleroch@gmail.com>
2024-05-22 14:46:21 +0200

    notfound page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Header from "@/components/header";
 import Swap from "@/components/swap";
 
-export default function Home() {
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   return (
     <main className="flex flex-col min-h-screen max-w-xl mx-auto">
       <Header />
